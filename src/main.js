8bf18f6a130e9791ccb140eed9ec3ccf61f2c9a2
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2016-01-14 20:59:26 -0200

    fix package

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/*!
+**  rey -- Framework based on React and Flux that looks like AngularJS.
+**  Copyright (c) 2016 Yuri Neves Silveira <http://yneves.com>
+**  Licensed under The MIT License <http://opensource.org/licenses/MIT>
+**  Distributed on <http://github.com/yneves/rey>
+*/
 // - -------------------------------------------------------------------- - //
 
 "use strict";
